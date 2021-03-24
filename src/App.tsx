9033bf0ffc9c1371e4672b98dcc9047db7450e69
Amundo81 <amund.helgesen.meling@gmail.com>
2021-03-24 14:06:42 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/gopher_heimdallr.png" className="App-logo" alt="logo" />
+        <p>
+          Goheimdallr
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/goheimdallr"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github Site
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
